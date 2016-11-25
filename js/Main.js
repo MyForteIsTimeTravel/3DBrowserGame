a1e8a7b508fe091c9e352dec8fcb502427bb4278
Ryan Needham <RyanNeedham@Outlook.com>
2016-11-25 13:13:44 +0000

    added proper lighting to scene

--- js/Main.js
+++ js/Main.js
@@ -42,6 +42,8 @@ scene.add(camera);
 
 // start rendering
 renderer.setSize(WIDTH, HEIGHT);
+renderer.shadowMapEnabled = true;
+renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 // attach to container
 container.appendChild(renderer.domElement);
@@ -50,15 +52,16 @@ container.appendChild(renderer.domElement);
  * Lighting Stuff
  * * * * * * * * * * * * * * * */
 const pointLight = new THREE.PointLight(0xFFFFFF);
-
 pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
-
+pointLight.position.y = 300;
+pointLight.position.z = 145;
+pointLight.rotation = 45 * (Math.PI / 180);
+pointLight.castShadow = true;
+pointLight.shadowDarkness = 0.5;
 scene.add(pointLight);
 
 /* * * * * * * * * * * * * * * *
- * Sphere Object
+ * World Objects
  * * * * * * * * * * * * * * * */
 const RADIUS    = 50;
 const SEGMENTS  = 16;
@@ -70,7 +73,7 @@ const sphereOne = new THREE.Mesh(
 );
 
 const sphereTwo = new THREE.Mesh(
-    new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS),  // Mesh
+    new THREE.CubeGeometry(48, 48, 48),  // Mesh
     new THREE.MeshLambertMaterial({color: 0xA52A2A})    // shader
 );
 
@@ -79,19 +82,38 @@ const sphereThree = new THREE.Mesh(
     new THREE.MeshLambertMaterial({color: 0xA52A2A})    // shader
 );
 
-sphereOne.position.y = 24;
+sphereOne.castShadow = true;
+sphereTwo.receiveShadow = true;
+sphereOne.position.y = 46;
 sphereOne.position.z = -300;
 scene.add(sphereOne);
 
+sphereTwo.castShadow = true;
+sphereTwo.receiveShadow = true;
 sphereTwo.position.x -= 200;
-sphereTwo.position.y = 18;
+sphereTwo.position.y = 40;
 sphereTwo.position.z = -500;
 scene.add(sphereTwo);
 
+sphereThree.castShadow = true;
+sphereThree.receiveShadow = true
 sphereThree.position.x += 200;
-sphereThree.position.y = 18;
+sphereThree.position.y = 40;
 sphereThree.position.z = -500;
 scene.add(sphereThree);
+
+const floor = new THREE.Mesh ( 
+    new THREE.PlaneGeometry(WIDTH * 2, WIDTH * 2, 50, 50), 
+    // Wireframe and Fill
+    new THREE.MeshLambertMaterial( { color: 0xAAAAAA, side: THREE.DoubleSide})
+);
+
+floor.receiveShadow = true;
+
+floor.position.y = -20;
+floor.rotation.x = 90 * (Math.PI / 180);
+
+scene.add(floor);
 
 /* * * * * * * * * * * * * * * *
  * Handle Input
@@ -138,6 +160,8 @@ function update () {
     
     if (mouseX > centralX + deadZone) { camera.rotation.y -= 0.0075;}
     if (mouseX < centralX - deadZone) { camera.rotation.y += 0.0075;}
+    //if (mouseY > centralY + deadZone) { camera.rotation.x -= 0.0075;}
+    //if (mouseY < centralY - deadZone) { camera.rotation.x += 0.0075;}
     
     // Draw the scene
     renderer.render(scene, camera); 
