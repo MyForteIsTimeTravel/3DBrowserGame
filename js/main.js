1383e803beb6e8e48b375dabd37a42e2486fc01a
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-11-26 12:06:18 +0000

    Added some extra pieces of geometry

--- js/main.js
+++ js/main.js
@@ -31,7 +31,8 @@ const scene      = new THREE.Scene()
 scene.background = new THREE.Color(0x202020)
 scene.add(camera)
 
-camera.position.z = 100
+camera.rotation.y = -10 * (Math.PI / 180)
+camera.position.z = 360
 
 renderer.setSize(WIDTH, HEIGHT)
 renderer.shadowMap.enabled = true
@@ -68,7 +69,7 @@ const object_1 = new THREE.Mesh(
 object_1.castShadow     = true
 object_1.receiveShadow  = true
 object_1.position.y     = 46
-object_1.position.z     = -200
+object_1.position.z     = -100
 scene.add(object_1);
 
 const object_2 = new THREE.Mesh(
@@ -80,7 +81,7 @@ object_2.castShadow     = true
 object_2.receiveShadow  = true
 object_2.position.x    -= 200
 object_2.position.y     = 40
-object_2.position.z     = -400
+object_2.position.z     = -200
 scene.add(object_2)
 
 const object_3 = new THREE.Mesh(
@@ -92,8 +93,20 @@ object_3.castShadow     = true
 object_3.receiveShadow  = true
 object_3.position.x    += 200
 object_3.position.y     = 40
-object_3.position.z     = -400
+object_3.position.z     = -200
 scene.add(object_3);
+
+const object_4 = new THREE.Mesh (
+    new THREE.TorusGeometry( 20, 6, 32, 200 ),
+    new THREE.MeshLambertMaterial({color: 0x111111})
+)
+
+object_4.castShadow = true
+object_4.receiveShadow = true
+object_4.position.x += 300 
+object_4.position.y = 60
+object_4.position.z = -40
+scene.add(object_4)
 
 const floor = new THREE.Mesh ( 
     new THREE.PlaneGeometry(1500, 1500, 50, 50), 
@@ -120,6 +133,7 @@ function update () {
         object_1.position.y += Math.cos(tick / 10)
         object_2.rotation.y += 0.005
         object_3.rotation.y -= 0.005
+        object_4.rotation.y += 0.05
 
         // Draw the scene
         renderer.render(scene, camera)
